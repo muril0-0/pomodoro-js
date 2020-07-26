54f50e1c5ffcb05e51116585a80bbee0aa0ebc54
Murilo <muriloholiveira@hotmail.com>
2020-07-26 00:51:47 -0300

    :hammer: Simple Counter

--- script.js
+++ script.js
@@ -1,26 +1,62 @@
-//Logic
+window.addEventListener("load", () => {
+  const workDisplay = document.querySelector(".work-display");
+  const workTime = document.querySelector(".work-time");
+  const upButton = document.querySelector(".up");
+  const downButton = document.querySelector(".down");
+  const playButton = document.querySelector(".play");
+  const pauseButton = document.querySelector(".pause");
+  const stopButton = document.querySelector(".stop");
+  let defaultTime = 25;
+  let paused = true;
 
-let time = 25;
-let paused = true;
+  workTime.value = defaultTime;
+  workDisplay.value = workTime.value;
 
-const initiate = setInterval(() => {
-  if (!pause) {
-    time--;
+  upButton.addEventListener("click", () => {
+    defaultTime++;
+    workTime.value = defaultTime;
+    workDisplay.value = workTime.value;
+  });
+
+  downButton.addEventListener("click", () => {
+    defaultTime--;
+    workTime.value = defaultTime;
+    workDisplay.value = workTime.value;
+  });
+
+  playButton.addEventListener("click", () => {
+    start();
+  });
+
+  pauseButton.addEventListener("click", () => {
+    pause();
+  });
+
+  stopButton.addEventListener("click", () => {
+    stop();
+  });
+
+  //Logic
+  const initiate = setInterval(() => {
+    if (!paused) {
+      workDisplay.value--;
+    }
+  }, 1000);
+  return () => {
+    clearInterval(initiate);
+  };
+
+  function start() {
+    paused = false;
   }
-}, 1000);
-return () => {
-  clearInterval(initiate);
-};
 
-function start() {
-  paused = false;
-}
+  function pause() {
+    paused = true;
+  }
 
-function pause() {
-  paused = true;
-}
-
-function stop() {
-  paused = true;
-  time = 25;
-}
+  function stop() {
+    paused = true;
+    workDisplay.value = 25;
+    workTime.value = 25;
+  }
+});
