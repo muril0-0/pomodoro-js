e641c2131be460f5cc87d87b878bc66f4f204c6d
Murilo <muriloholiveira@hotmail.com>
2020-07-29 12:15:40 -0300

    :fire: Counter without loop

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ window.addEventListener("load", () => {
   const relaxDisplay = document.querySelector(".relax-display");
   const focusTime = document.querySelector(".focus-time");
   const relaxTime = document.querySelector(".relax-time");
-  const upButton = document.querySelector(".up");
-  const downButton = document.querySelector(".down");
+  const upFocus = document.querySelector(".up-focus");
+  const downFocus = document.querySelector(".down-focus");
+  const upRelax = document.querySelector(".up-relax");
+  const downRelax = document.querySelector(".down-relax");
   const playButton = document.querySelector(".play");
   const pauseButton = document.querySelector(".pause");
   const stopButton = document.querySelector(".stop");
@@ -20,14 +22,40 @@ window.addEventListener("load", () => {
   focusTime.value = `${("00" + Math.floor(newFocus / 60)).slice(-2)}`;
   relaxTime.value = `${("00" + Math.floor(newRelax / 60)).slice(-2)}`;
 
-  upButton.addEventListener("click", () => {
-    newFocus = newFocus + 60;
-    focusTime.value = `${Math.floor(newFocus / 60)}`;
+  upFocus.addEventListener("click", () => {
+    if (newFocus < 60 * 60) {
+      newFocus = newFocus + 60;
+    } else {
+      newFocus = 0;
+    }
+    focusTime.value = `${("00" + Math.floor(newFocus / 60)).slice(-2)}`;
   });
 
-  downButton.addEventListener("click", () => {
-    newFocus = newFocus - 60;
-    focusTime.value = `${Math.floor(newFocus / 60)}`;
+  downFocus.addEventListener("click", () => {
+    if (newFocus > 0) {
+      newFocus = newFocus - 60;
+    } else {
+      newFocus = 0;
+    }
+    focusTime.value = `${("00" + Math.floor(newFocus / 60)).slice(-2)}`;
+  });
+
+  upRelax.addEventListener("click", () => {
+    if (newRelax < 60 * 60) {
+      newRelax = newRelax + 60;
+    } else {
+      newRelax = 0;
+    }
+    relaxTime.value = `${("00" + Math.floor(newRelax / 60)).slice(-2)}`;
+  });
+
+  downRelax.addEventListener("click", () => {
+    if (newRelax > 0) {
+      newRelax = newRelax - 60;
+    } else {
+      newRelax = 0;
+    }
+    relaxTime.value = `${("00" + Math.floor(newRelax / 60)).slice(-2)}`;
   });
 
   playButton.addEventListener("click", () => {
@@ -43,16 +71,33 @@ window.addEventListener("load", () => {
   });
 
   //Logic
-  const initiate = setInterval(() => {
-    if (!paused) {
+  const initiateFocus = setInterval(() => {
+    if (!paused && newFocus > 0) {
       newFocus--;
       focusDisplay.value = `${Math.floor(newFocus / 60)}:${(
         "00" + Math.floor(newFocus % 60)
       ).slice(-2)}`;
+
+      relaxDisplay.value = `${Math.floor(newRelax / 60)}:${(
+        "00" + Math.floor(newRelax % 60)
+      ).slice(-2)}`;
+      if (newFocus == 0) {
+        const initiateRelax = setInterval(() => {
+          if (!paused && newRelax > 0) {
+            newRelax--;
+            relaxDisplay.value = `${Math.floor(newRelax / 60)}:${(
+              "00" + Math.floor(newRelax % 60)
+            ).slice(-2)}`;
+          }
+        }, 1000);
+        return () => {
+          clearInterval(initiateRelax);
+        };
+      }
     }
   }, 1000);
   return () => {
-    clearInterval(initiate);
+    clearInterval(initiateFocus);
   };
 
   function start() {
@@ -70,3 +115,5 @@ window.addEventListener("load", () => {
     relaxDisplay.value = "--:--";
   }
 });
+
+function startFocus() {}
